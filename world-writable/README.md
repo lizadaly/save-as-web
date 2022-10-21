1df16786fad996def2aef8bb1dc384cd214c4fc7
Liza Daly <lizadaly@gmail.com>
2022-10-21 11:12:07 -0400

    This dumb thing works

---
+++ world-writable/README.md
@@ -0,0 +1,12 @@
+# The world-writable book
+
+Clone or copy this repo.
+
+Open `index.html` by double-clicking on it. You don't need a web server!
+
+Click "Save changes".
+
+When the directory dialog opens, select this directory. The browser will ask you to confirm that the page can write to files in this directory. Let it!
+
+The `index.html` will be updated in place with your changes. What a way to live!
+
--- world-writable/app.js
+++ world-writable/app.js
@@ -1,14 +1,23 @@
-document.querySelector("#edit").addEventListener('click', async () => {
+document.querySelector('#edit').addEventListener('click', () => {
+
+    document.querySelector('article').setAttribute('contenteditable', true)
+    document.body.setAttribute('data-edited', true)
+
+})
+
+document.querySelector("#save").addEventListener('click', async () => {
     // Begin editing
-    const dir = await window.showDirectoryPicker()
-    const fh = await window.showSaveFilePicker({startIn: dir})
+    const fh = await window.showSaveFilePicker()
+
+    document.querySelector('article').removeAttribute('contenteditable')
+    document.body.removeAttribute('data-edited')
 
     // Get the current content of this HTML file
     const s = new XMLSerializer()
     const d = document
     const contents = s.serializeToString(d)
 
-
+    // Write it back out
     const writable = await fh.createWritable();
     await writable.write(contents);
     await writable.close();
