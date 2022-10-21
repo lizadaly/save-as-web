56c6f42119795822d4623400623765c66f5dab08
Liza Daly <lizadaly@gmail.com>
2022-10-21 10:46:39 -0400

    Serialized same file

--- world-writable/app.js
+++ world-writable/app.js
@@ -1,12 +1,18 @@
 document.querySelector("#edit").addEventListener('click', async () => {
     // Begin editing
     const dir = await window.showDirectoryPicker()
-    const fh = await window.showSaveFilePicker()
+    const fh = await window.showSaveFilePicker({startIn: dir})
 
     // Get the current content of this HTML file
     const s = new XMLSerializer()
     const d = document
-    const out = s.serializeToString(d)
-    console.log(out)
+    const contents = s.serializeToString(d)
 
+
+    const writable = await fh.createWritable();
+    await writable.write(contents);
+    await writable.close();
+
+    // Refresh
+    location.reload()
 })
